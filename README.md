a9e55adb7e462519e08453fb9aa61078aa7e351d
Daniel Leech <dan.t.leech@gmail.com>
2018-11-04 20:32:02 +0000

    no tests

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Package Manager Extension
-==================
+=========================
 
 [![Build Status](https://travis-ci.org/phpactor/pacman-extension.svg?branch=master)](https://travis-ci.org/phpactor/pacman-extension)
+
+This is both a Phpactor extension and a standalone application for analyzing
+packages.
--- lib/Command/PackageMetricsListCommand.php
+++ lib/Command/PackageMetricsListCommand.php
@@ -7,13 +7,12 @@ use Phpactor\Pacman\Model\PackageMetrics;
 use Phpactor\Pacman\Model\PackageMetrics\PackageFinder;
 use Phpactor\Pacman\Model\Scanner;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PackageMetricsListCommand extends Command
+class PackageMetricsCommand extends Command
 {
     const NAME = 'pattern';
 
@@ -36,23 +35,17 @@ class PackageMetricsListCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $output->writeln('<info>Ca = Afferent Couplings:</> Number of classes in other packages referring to this class');
+        $output->writeln('<info>Ce = Efferent Couplings:</> Number of classes in package referring to classes in other packages');
+        $output->writeln('<info>A = Abstractness:</> Ratio of abstract to concrete classes. 1 is completely abstract');
+        $output->writeln('<info>I = Instability:</> Ration of efferent to afferent couplings. 1 is completely unstable package');
         $table = new Table($output);
         $table->setHeaders([
             'name', 'version', 'Ca ☐←', 'Ce ☐→', 'A', 'I',
         ]);
-        $output->writeln('Scanning files...');
-        $progress = new ProgressBar($output);
-        $generator = $this->scanner->scan($input->getArgument(self::NAME));
-        foreach ($generator as $tick) {
-            $progress->advance();
-        }
-        $output->write(PHP_EOL);
-
-        foreach ($generator->getReturn() as $metrics) {
+        foreach ($this->scanner->scan($input->getArgument(self::NAME)) as $metrics) {
             $this->addMetricsRow($table, $metrics);
         }
-
-        $this->writeLegend($output);
         $table->render();
     }
 
@@ -66,13 +59,5 @@ class PackageMetricsListCommand extends Command
             number_format($metrics->abstractness(), 2),
             number_format($metrics->instability(), 2)
         ]);
-    }
-
-    private function writeLegend(OutputInterface $output)
-    {
-        $output->writeln('<info>Ca = Afferent Couplings:</> Number of classes in other packages referring to this class');
-        $output->writeln('<info>Ce = Efferent Couplings:</> Number of classes in package referring to classes in other packages');
-        $output->writeln('<info>A = Abstractness:</> Ratio of abstract to concrete classes. 1 is completely abstract');
-        $output->writeln('<info>I = Instability:</> Ratio of efferent to afferent couplings. 1 is completely unstable package');
     }
 }
